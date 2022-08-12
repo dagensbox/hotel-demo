76cc53fd7b7d49758dd91cb96a9487e9495dc7de
box <1214142612@qq.com>
2022-08-12 20:31:34 +0800

    hotel-demo document操作

--- src/test/java/cn/itcast/hotel/HotelDocumentTest.java
+++ src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -1,6 +1,11 @@
 package cn.itcast.hotel;
 
+import cn.itcast.hotel.pojo.Hotel;
+import cn.itcast.hotel.pojo.HotelDoc;
+import cn.itcast.hotel.service.IHotelService;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.Result;
+import co.elastic.clients.elasticsearch.core.*;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
@@ -8,20 +13,91 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
+@SpringBootTest
 public class HotelDocumentTest {
+    @Autowired
+    private IHotelService hotelService;
 
     private RestClient restClient;
+    private ElasticsearchTransport transport;
     private ElasticsearchClient client;
+
+    // 新增一条文档
+    @Test
+    void testAddDocument() throws IOException {
+        // 根据id查询酒店数据
+        Hotel hotel = hotelService.getById(60398);
+        // 转换为文档类型
+        HotelDoc hotelDoc = new HotelDoc(hotel);
+        // 发送请求
+        IndexResponse response = client.index(request ->
+                request.index("hotel").id(hotel.getId().toString()).document(hotelDoc)
+        );
+        System.out.println(response);
+    }
+
+    // 根据id查询文档
+    @Test
+    void testGetDocumentById() throws IOException {
+        GetResponse<HotelDoc> response = client.get(request -> request.index("hotel").id("60398"), HotelDoc.class);
+        System.out.println("id -->" + response.id());
+        System.out.println("index -->" + response.index());
+        System.out.println("routing -->" + response.routing());
+        System.out.println("all -->" + response);
+        HotelDoc source = response.source();
+        System.out.println("-----------------------------");
+        System.out.println(source);
+    }
+
+    //全量更新 即再写一次
+    @Test
+    void testUpdateDocAll() throws IOException {
+        // 根据id查询酒店数据
+        Hotel hotel = hotelService.getById(60398);
+        // 转换为文档类型
+        HotelDoc hotelDoc = new HotelDoc(hotel);
+        hotelDoc.setCity("重庆");
+        // 发送请求
+        IndexResponse response = client.index(request ->
+                request.index("hotel").id(hotel.getId().toString()).document(hotelDoc)
+        );
+        System.out.println(response);
+    }
+
+    //全量更新 仅更新部分字段
+    @Test
+    void testUpdateDoc() throws IOException {
+        HotelDoc hotelDoc = new HotelDoc();
+        hotelDoc.setCity("New York");
+        hotelDoc.setPrice(999);
+        hotelDoc.setStarName("五钻石");
+        UpdateResponse<HotelDoc> response = client.update(req ->
+                        req.index("hotel").id(60398 + "").doc(hotelDoc)
+                , HotelDoc.class);
+        Result result = response.result();
+        System.out.println(result);
+        System.out.println(response);
+    }
+
+    //删除文档
+    @Test
+    void testDeleteById() throws IOException {
+        DeleteResponse response = client.delete(req -> req.index("hotel").id("60398"));
+        System.out.println(response);
+    }
 
     @BeforeEach
     void setUp() {
         restClient = RestClient.builder(
                 new HttpHost("localhost", 9200)).build();
         // Create the transport with a Jackson mapper
-        ElasticsearchTransport transport = new RestClientTransport(
+        transport = new RestClientTransport(
                 restClient, new JacksonJsonpMapper());
         // And create the API client
         client = new ElasticsearchClient(transport);
@@ -29,6 +105,7 @@ public class HotelDocumentTest {
 
     @AfterEach
     void tearDown() throws IOException {
-        this.restClient.close();
+        transport.close();
+        restClient.close();
     }
 }
