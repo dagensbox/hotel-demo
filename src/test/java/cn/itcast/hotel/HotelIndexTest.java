115f3811461054e5ed5236c6752ee855a39464a1
box <1214142612@qq.com>
2022-08-12 18:57:41 +0800

    hotel-demo index操作测试

---
+++ src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -0,0 +1,140 @@
+package cn.itcast.hotel;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.mapping.Property;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexResponse;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.endpoints.BooleanResponse;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Slf4j
+public class HotelIndexTest {
+
+    private ElasticsearchClient client;
+
+
+    //创建索引 -不指定 mapping
+    @Test
+    void testCreateIndex() throws IOException {
+        CreateIndexResponse indexResponse = client.indices().create(c -> c.index("test1"));
+        System.out.println(indexResponse);
+    }
+
+
+    // 创建索引 -指定mapping
+    @Test
+    void testCreateIndexWithMapping() throws IOException {
+        CreateIndexResponse indexResponse = client.indices()
+                .create(c ->
+                        c.index("test2").mappings(typeMapping ->
+                                typeMapping.properties("name", objectBuilder ->
+                                        objectBuilder.text(textProperty -> textProperty.fielddata(true))
+                                ).properties("age", objectBuilder ->
+                                        objectBuilder.integer(integerProperty -> integerProperty.index(true))
+                                )
+                        )
+                );
+        System.out.println(indexResponse);
+    }
+
+
+    // 创建索引 -指定mapping map
+    @Test
+    void testCreateIndexWithMappingMap() throws IOException {
+        Map<String, Property> map = new HashMap<>();
+        map.put("name", Property.of(property -> property.text(objectBuilder ->
+                objectBuilder.fielddata(true).index(true).analyzer("ik_max_word"))));
+        map.put("age", Property.of(property -> property.integer(objectBuilder ->
+                objectBuilder)));
+        CreateIndexResponse indexResponse = client.indices()
+                .create(c -> c.index("test3").mappings(typeMapping -> typeMapping.properties(map)));
+        System.out.println(indexResponse);
+    }
+
+
+    // 创建索引 -指定mapping map
+    @Test
+    void testCreateIndexOfHotel() throws IOException {
+        CreateIndexResponse indexResponse = client.indices()
+                .create(c -> c.index("hotel").mappings(typeMapping -> typeMapping.properties(getHotelMap())));
+        System.out.println(indexResponse);
+    }
+
+
+    private Map<String, Property> getHotelMap() {
+        Map<String, Property> map = new HashMap<>();
+        map.put("id", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder)));
+        map.put("name", Property.of(property -> property.text(objectBuilder ->
+                objectBuilder.analyzer("ik_max_word").copyTo("all"))));
+        map.put("address", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder.index(false))));
+        map.put("price", Property.of(property -> property.integer(objectBuilder ->
+                objectBuilder)));
+        map.put("score", Property.of(property -> property.integer(objectBuilder ->
+                objectBuilder)));
+        map.put("brand", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder.copyTo("all"))));
+        map.put("city", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder.copyTo("all"))));
+        map.put("startName", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder)));
+        map.put("business", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder)));
+        map.put("location", Property.of(property -> property.geoPoint(objectBuilder ->
+                objectBuilder)));
+        map.put("pic", Property.of(property -> property.keyword(objectBuilder ->
+                objectBuilder.index(false))));
+        map.put("all", Property.of(property -> property.text(objectBuilder ->
+                objectBuilder.analyzer("ik_max_word"))));
+        return map;
+    }
+
+    // 删除索引
+    @Test
+    void testDeleteIndex() throws IOException {
+        DeleteIndexResponse response = client.indices().delete(c -> c.index("hotel"));
+        System.out.println(response);
+    }
+
+    @Test
+    void testExistsIndex() throws IOException {
+        BooleanResponse response = client.indices().exists(c -> c.index("hotel"));
+        System.out.println(response.value());
+    }
+
+    @Test
+    void testInit() {
+        System.out.println(client);
+    }
+
+
+    @BeforeEach
+    void setUp() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+        // And create the API client
+        client = new ElasticsearchClient(transport);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.client.shutdown();
+    }
+}
